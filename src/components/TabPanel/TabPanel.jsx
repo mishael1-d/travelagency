f0e185bc5d8ee05efde413c972817bb237b67c28
Mishael Dada <Mishaeldada@gmail.com>
2022-06-26 20:55:28 +0100

    Tab panel build2

--- src/components/TabPanel/TabPanel.jsx
+++ src/components/TabPanel/TabPanel.jsx
@@ -1,29 +1,49 @@
 import * as React from "react";
+import "./TabPanel.css";
 
-export  const FirstTab = () => {
+export const FirstTab = () => {
   return (
     <div className="firstTab">
       <p>First Tab</p>
     </div>
-  )
-}
-export  const SecondTab = () => {
+  );
+};
+export const SecondTab = () => {
   return (
     <div className="secondTab">
       <p>Second Tab</p>
     </div>
-  )
-}
+  );
+};
 
+function TabPanel() {
+  const [activeTab, setActiveTab] = React.useState("tab1");
 
-function TabPanel(props) {
+  const handleTab1 = () => {
+    setActiveTab("tab1");
+  };
+  const handleTab2 = () => {
+    setActiveTab("tab2");
+  };
   return (
     <div className="tabpanel__container">
       <ul className="tab-nav__container">
-        <li>Tab 1</li>
-        <li>Tab 2</li>
+        <li
+          className={activeTab === "tab1" ? "active" : ""}
+          onClick={handleTab1}
+        >
+          Tab 1
+        </li>
+        <li
+          className={activeTab === "tab2" ? "active" : ""}
+          onClick={handleTab2}
+        >
+          Tab 2
+        </li>
       </ul>
-      <div className="tab-content__container"></div>
+      <div className="tab-content__container">
+        {activeTab === "tab1" ? <FirstTab /> : <SecondTab />}
+      </div>
     </div>
   );
 }
