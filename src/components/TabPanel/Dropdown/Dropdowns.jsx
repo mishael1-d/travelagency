39c1da05f9805ebc8ea2ee1ad4b2a3edda9e21fc
mishael dada <mishaeldada@gmail.com>
2022-06-27 09:35:42 +0100

    update

--- src/components/TabPanel/Dropdown/Dropdowns.jsx
+++ src/components/TabPanel/Dropdown/Dropdowns.jsx
@@ -10,7 +10,7 @@ export default function BasicSelect({ type }) {
         className="select-btn"
         classNamePrefix="select-dropdown"
         defaultValue={options1[0]}
-        isSearchable={true}
+        isSearchable={false}
         name="Trip Type"
         options={options1}
       />
@@ -18,7 +18,7 @@ export default function BasicSelect({ type }) {
         className="select-btn"
         classNamePrefix="select-dropdown"
         defaultValue={options2[0]}
-        isSearchable={true}
+        isSearchable={false}
         name="Trip Type"
         options={options2}
       />
@@ -26,7 +26,7 @@ export default function BasicSelect({ type }) {
         className="select-btn"
         classNamePrefix="select-dropdown"
         defaultValue={options3[0]}
-        isSearchable={true}
+        isSearchable={false}
         name="Trip Type"
         options={options3}
       />
@@ -36,10 +36,9 @@ export default function BasicSelect({ type }) {
 
 
 const options1 = [
-  {value: 'london', label: 'London'},
-  {value: 'ghana', label: 'Ghana'},
-  {value: 'india', label: 'India'},
-  {value: 'togo', label: 'Togo'},
+  {value: 'round-trip', label: 'Round Trip'},
+  {value: 'one way', label: 'One Way'},
+  {value: 'multi city', label: 'Multi City'}
 ]
 const options2 = [
   {value: '1 Passenger', label: '1 Passenger'},
@@ -49,7 +48,7 @@ const options2 = [
 ]
 const options3 = [
   {value: 'Economy', label: 'Economy'},
-  {value: 'ghana', label: 'Ghana'},
-  {value: 'india', label: 'India'},
-  {value: 'togo', label: 'Togo'},
+  {value: 'premiuom economy', label: 'Premiun Economy'},
+  {value: 'business', label: 'Business'},
+  {value: 'first class', label: 'First Class'},
 ]
